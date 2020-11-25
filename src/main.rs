556cab8ec7790be39f20ed0e4066bc25d461dcf2
Brent Gardner <bgardner@squarelabs.net>
2020-11-25 12:22:39 -0700

    Make current

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use khronos_egl::{create_context, get_current_display, NO_CONTEXT, CONTEXT_CLIENT_VERSION, Config, EGLConfig, DEFAULT_DISPLAY, get_display, initialize, choose_first_config};
+use khronos_egl::{choose_first_config, Config, CONTEXT_CLIENT_VERSION, create_context, DEFAULT_DISPLAY, EGLConfig, get_current_display, get_display, initialize, make_current, NO_CONTEXT};
 
 fn main() {
+
+    // egl init
     let display = get_display(DEFAULT_DISPLAY).expect("Need a display!");
     let attributes = [khronos_egl::NONE];
     let res = initialize(display).expect("Can't initialize");
@@ -9,5 +11,9 @@ fn main() {
         .expect("unable to choose an EGL configuration")
         .expect("no EGL configuration found");
     let ctx = create_context(display, config, None, &attributes).expect("Need a context!");
-    println!("Hello, world: {:?}", ctx);
+    println!("EGL context={:?}", ctx);
+    make_current(display, None, None, Some(ctx)).expect("Can't make current");
+
+    // gles
+    
 }
