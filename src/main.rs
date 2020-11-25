33100ccaa6d86e66b55602d5a3920510be8cfec8
Brent Gardner <bgardner@squarelabs.net>
2020-11-25 16:10:13 -0700

    Can get pixels

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use khronos_egl::{choose_first_config, Config, CONTEXT_CLIENT_VERSION, create_context, DEFAULT_DISPLAY, EGLConfig, get_current_display, get_display, initialize, make_current, NO_CONTEXT};
-use rs_gles3::{glGenBuffers, GLuint, glGenVertexArrays, glBindVertexArray, glBindBuffer, GL_ARRAY_BUFFER, glBufferData, GL_STATIC_DRAW, glEnableVertexAttribArray, glVertexAttribPointer, GL_FLOAT, GL_FALSE, GLfloat, glClear, GL_COLOR_BUFFER_BIT, GLenum, GLint, glCreateShader, glShaderSource, glCompileShader, glGetShaderiv, GL_COMPILE_STATUS, glDeleteShader, GLchar, GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, glCreateProgram, glAttachShader, glLinkProgram, glGetProgramiv, GL_LINK_STATUS, glDeleteProgram, glDetachShader, glGetError, GL_INVALID_ENUM, GL_INVALID_VALUE, GL_INVALID_OPERATION, GL_OUT_OF_MEMORY, GL_INVALID_FRAMEBUFFER_OPERATION, GL_ELEMENT_ARRAY_BUFFER};
-use std::ptr::null;
 use std::ffi::c_void;
+use std::os::raw::c_char;
+use std::ptr::null;
+
 use anyhow::{anyhow, Context, Error};
+use khronos_egl::{choose_first_config, Config, CONTEXT_CLIENT_VERSION, create_context, create_pbuffer_surface, DEFAULT_DISPLAY, EGLConfig, get_current_display, get_display, initialize, make_current, NO_CONTEXT, GL_COLORSPACE, GL_COLORSPACE_SRGB};
+use rs_gles3::{GL_ARRAY_BUFFER, GL_COLOR_BUFFER_BIT, GL_COMPILE_STATUS, GL_ELEMENT_ARRAY_BUFFER, GL_FALSE, GL_FLOAT, GL_FRAGMENT_SHADER, GL_INVALID_ENUM, GL_INVALID_FRAMEBUFFER_OPERATION, GL_INVALID_OPERATION, GL_INVALID_VALUE, GL_LINK_STATUS, GL_OUT_OF_MEMORY, GL_STATIC_DRAW, GL_TRIANGLES, GL_TRUE, GL_UNSIGNED_SHORT, GL_VERTEX_SHADER, glAttachShader, glBindBuffer, glBindVertexArray, glBufferData, GLchar, glClear, glCompileShader, glCreateProgram, glCreateShader, glDeleteProgram, glDeleteShader, glDetachShader, glDrawElements, glDrawElementsInstanced, glEnableVertexAttribArray, GLenum, GLfloat, glGenBuffers, glGenVertexArrays, glGetError, glGetProgramiv, glGetShaderiv, glGetUniformLocation, GLint, glLinkProgram, glReadPixels, glShaderSource, GLuint, glUniformMatrix4fv, glUseProgram, glVertexAttribPointer, glCopyTexImage2D, GL_RGB_INTEGER, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, glFinish, glPixelStorei, GL_UNPACK_ALIGNMENT, glBindFramebuffer, glViewport, glClearColor, GL_RGBA};
+use std::fs::File;
+use std::io::Write;
 
 fn main() -> Result<(), Error> {
     unsafe {
@@ -16,7 +20,19 @@ fn main() -> Result<(), Error> {
             .expect("no EGL configuration found");
         let ctx = create_context(display, config, None, &attributes).expect("Need a context!");
         println!("EGL context={:?}", ctx);
-        make_current(display, None, None, Some(ctx)).expect("Can't make current");
+
+        // create surface
+        let width = 800;
+        let height = 600;
+        let attributes = [
+            khronos_egl::WIDTH, width.clone(),
+            khronos_egl::HEIGHT, height.clone(),
+            khronos_egl::TEXTURE_FORMAT, khronos_egl::TEXTURE_RGBA,
+            khronos_egl::TEXTURE_TARGET, khronos_egl::TEXTURE_2D,
+            khronos_egl::NONE,
+        ];
+        let surface = create_pbuffer_surface(display, config, &attributes).expect("Couldn't create pbuffer");
+        make_current(display, Some(surface.clone()), Some(surface.clone()), Some(ctx)).expect("Can't make current");
 
         // https://github.com/AlexCharlton/hello-modern-opengl/blob/master/hello-gl.c
         let vert_shader = "\
@@ -39,11 +55,12 @@ void main(){\n\
 }\0";
 
         let program_id = make_shader(vert_shader, frag_shader)?;
+        let mvp_location = glGetUniformLocation(program_id, "mvp".as_ptr() as *const i8);
 
         let mut vertex_buffer_data = [
             -1.0f32, -1.0f32, -1.0f32, 0.0f32, 0.0f32, 1.0f32,
             1.0f32, -1.0f32, -1.0f32, 0.0f32, 1.0f32, 0.0f32,
-            1.0f32,  1.0f32, -1.0f32, 1.0f32, 0.0f32, 0.0f32
+            1.0f32, 1.0f32, -1.0f32, 1.0f32, 0.0f32, 0.0f32
         ];
 
         // 250
@@ -75,10 +92,48 @@ void main(){\n\
         check_error().context("Cannot set vertex attrib pointer 2")?;
 
         // 267
-        let index_buffer_data = [0u16, 1u16, 2u16 ];
+        let index_buffer_data = [0u16, 1u16, 2u16];
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, (index_buffer_data.len() * 2) as i64, index_buffer_data.as_ptr() as *const c_void, GL_STATIC_DRAW);
         check_error().context("Cannot bind index buffer")?;
+
+        // 309
+        // glClear(GL_COLOR_BUFFER_BIT);
+        // check_error().context("Cannot clear!")?;
+
+        // 277
+        let mvp = [
+            1f32, 0f32, 0f32, 0f32,
+            0f32, 1f32, 0f32, 0f32,
+            0f32, 0f32, 1f32, 0f32,
+            0f32, 0f32, 0f32, 1f32,
+        ];
+        glUseProgram(program_id);
+        check_error().context("Cannot use shader!")?;
+        glUniformMatrix4fv(mvp_location, 1, GL_TRUE as u8, mvp.as_ptr() as *const f32);
+        check_error().context("Cannot set MVP!")?;
+        glBindVertexArray(vertex_array_id);
+        check_error().context("Cannot bind vert array!")?;
+
+        glViewport(0, 0, width, height);
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        check_error().context("Cannot clear!")?;
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, null());
+        check_error().context("Cannot draw!")?;
+        glFinish();
+        check_error().context("Cannot finish!")?;
+
+        let mut pixels = vec![0u8; (width * height) as usize * 4];
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        check_error().context("Cannot set pixel store mode!")?;
+        glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.as_mut_ptr() as *mut c_void);
+        check_error().context("Cannot get pixels!")?;
+
+        // Save
+        let mut file = File::create("/data/pic.raw")?;
+        file.write_all(&pixels[..])?;
     }
 
     Ok(())
