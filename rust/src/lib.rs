c89e11ae910108c77ba1f9e4aaee63a1dee906c6
Brent Gardner <bgardner@squarelabs.net>
2020-11-27 09:50:42 -0700

    Can bindgen

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,23 +1,27 @@
 #![cfg(target_os = "android")]
 #![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
 
 extern crate android_logger;
 
 use std::ffi::c_void;
+use std::fs::File;
+use std::io::{Read, Write};
 use std::os::raw::c_char;
 use std::ptr::null;
 
 use anyhow::{anyhow, Context, Error};
-use khronos_egl::{choose_first_config, Config, CONTEXT_CLIENT_VERSION, create_context, create_pbuffer_surface, DEFAULT_DISPLAY, EGLConfig, get_current_display, get_display, initialize, make_current, NO_CONTEXT, GL_COLORSPACE, GL_COLORSPACE_SRGB, swap_buffers, query_surface, create_pixmap_surface, choose_config};
-use rs_gles3::{GL_ARRAY_BUFFER, GL_COLOR_BUFFER_BIT, GL_COMPILE_STATUS, GL_ELEMENT_ARRAY_BUFFER, GL_FALSE, GL_FLOAT, GL_FRAGMENT_SHADER, GL_INVALID_ENUM, GL_INVALID_FRAMEBUFFER_OPERATION, GL_INVALID_OPERATION, GL_INVALID_VALUE, GL_LINK_STATUS, GL_OUT_OF_MEMORY, GL_STATIC_DRAW, GL_TRIANGLES, GL_TRUE, GL_UNSIGNED_SHORT, GL_VERTEX_SHADER, glAttachShader, glBindBuffer, glBindVertexArray, glBufferData, GLchar, glClear, glCompileShader, glCreateProgram, glCreateShader, glDeleteProgram, glDeleteShader, glDetachShader, glDrawElements, glDrawElementsInstanced, glEnableVertexAttribArray, GLenum, GLfloat, glGenBuffers, glGenVertexArrays, glGetError, glGetProgramiv, glGetShaderiv, glGetUniformLocation, GLint, glLinkProgram, glReadPixels, glShaderSource, GLuint, glUniformMatrix4fv, glUseProgram, glVertexAttribPointer, glCopyTexImage2D, GL_RGB_INTEGER, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, glFinish, glPixelStorei, GL_UNPACK_ALIGNMENT, glBindFramebuffer, glViewport, glClearColor, GL_RGBA, GL_DEPTH_BUFFER_BIT, glDisable, GL_CULL_FACE, GL_DEPTH, GL_DEPTH_TEST, glDrawArrays, glGetAttribLocation, GL_LINES, GL_PACK_ALIGNMENT, glValidateProgram, GL_POINTS, GL_VALIDATE_STATUS, glGenTextures, glTexImage2D, glBindTexture, GL_TEXTURE_2D, GL_RGB, glBindSampler, GL_TEXTURE0, glActiveTexture, glTexParameteri, GL_TEXTURE_WRAP_S, GL_REPEAT, GL_TEXTURE_WRAP_T, GL_TEXTURE_MIN_FILTER, GL_TEXTURE_MAG_FILTER, GL_LINEAR, GL_RG, GL_LUMINANCE_ALPHA, GL_TEXTURE1, GL_LUMINANCE, GL_TEXTURE2, glGenFramebuffers, GL_FRAMEBUFFER, glFramebufferTexture2D, GL_COLOR_ATTACHMENT0, glCheckFramebufferStatus, GL_FRAMEBUFFER_COMPLETE, GL_R8, GL_RED, glReadBuffer, GL_IMPLEMENTATION_COLOR_READ_FORMAT};
-use std::fs::File;
-use std::io::{Write, Read};
 use jni::JNIEnv;
 use jni::objects::{JObject, JString};
-use log::Level;
-use log::info;
+use khronos_egl::{choose_config, choose_first_config, Config, CONTEXT_CLIENT_VERSION, create_context, create_pbuffer_surface, create_pixmap_surface, DEFAULT_DISPLAY, EGLConfig, get_current_display, get_display, GL_COLORSPACE, GL_COLORSPACE_SRGB, initialize, make_current, NO_CONTEXT, query_surface, swap_buffers};
 use log::error;
-use image::io::Reader as ImageReader;
+use log::info;
+use log::Level;
+use std::mem::size_of;
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[no_mangle]
 pub extern fn Java_com_mersive_glconvert_MainActivity_init(
@@ -108,200 +112,14 @@ void main(){{\n\
     color = texture(tex_in, texcoord).r;\n\
 }}\n\0", ver);
 
-        info!("Compiling shader...");
-        let program_id = make_shader(vert_shader.as_str(), frag_shader.as_str()).context("Couldn't make shader")?;
-        info!("Getting parameters...");
-        let name = "vertex\0".as_ptr() as *const GLchar;
-        let vertex_loc = glGetAttribLocation(program_id, name);
+        info!("{} {}", vert_shader, frag_shader);
 
-        let name = "texcoordin\0".as_ptr() as *const GLchar;
-        let texcoord_loc = glGetAttribLocation(program_id, name);
-
-        let name = "tex_in\0".as_ptr() as *const GLchar;
-        let tex_in = glGetUniformLocation(program_id, name);
-        
-        info!("Have a program={} vertex_loc={} texcoord_loc={} tex_in={}", program_id, vertex_loc, texcoord_loc, tex_in);
-
-        // texture
-        let filename = format!("{}/thanksgiving.raw", path);
-        let mut f = File::open(&filename).context("no file found")?;
-        let metadata = std::fs::metadata(&filename).context("unable to read metadata")?;
-        let mut bytes = vec![0; metadata.len() as usize];
-        f.read(&mut bytes).context("buffer overflow")?;
-        info!("Read {} byte image", bytes.len());
-
-        // -------------------- y ouput texture
-        glActiveTexture(GL_TEXTURE1);
-        let mut yout: GLuint = 0;
-        glGenTextures(1, &mut yout);
-        glBindTexture(GL_TEXTURE_2D, yout);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_R8 as i32, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, null());
-        check_error().context("Cannot load texture")?;
-        glBindTexture(GL_TEXTURE_2D, 0);
-
-        // -------------------- uv ouput texture
-        glActiveTexture(GL_TEXTURE2);
-        let mut uvout: GLuint = 0;
-        glGenTextures(1, &mut uvout);
-        glBindTexture(GL_TEXTURE_2D, uvout);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE_ALPHA as i32, width / 2, height / 2, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, null());
-        check_error().context("Cannot load texture")?;
-        glBindTexture(GL_TEXTURE_2D, 0);
-
-        // -------------------- input texture
-        glActiveTexture(GL_TEXTURE0);
-        let mut thanksgiving: GLuint = 0;
-        info!("Generating texture...");
-        glGenTextures(1, &mut thanksgiving);
-        info!("Binding texture...");
-        glBindTexture(GL_TEXTURE_2D, thanksgiving);
-        info!("Setting texture parameters...");
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT as i32);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT as i32);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR as i32);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR as i32);
-        info!("Loading texture...");
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE_ALPHA as i32, width, height, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, bytes.as_ptr() as *const c_void);
-        info!("Binding sampler...");
-        glBindSampler(0, tex_in as u32);
-        check_error().context("Cannot load texture")?;
-
-        let mut vertex_buffer_data: Vec<f32> = vec![
-            -1.0, -1.0, 0., 0., 0.,
-            1.0, -1.0, 0., 1., 0.,
-            1.0, 1.0, 0., 1., 1.,
-            1.0, 1.0, 0., 1., 1.,
-            -1.0, 1.0, 0., 0., 1.,
-            -1.0, -1.0, 0., 0., 0.,
-        ];
-
-        // vert buf
-        let mut vertex_buffer: GLuint = 0;
-        glGenBuffers(1, &mut vertex_buffer);
-        if vertex_buffer == 0 { panic!("Invalid vertex buffer!"); }
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-        check_error().context("Cannot bind buffer!")?;
-        glBufferData(GL_ARRAY_BUFFER, (vertex_buffer_data.len() * 4) as i64, vertex_buffer_data.as_ptr() as *const c_void, GL_STATIC_DRAW);
-        check_error().context("Cannot set buffer data")?;
-
-        // vertex array
-        let mut vertex_array_id: GLuint = 0;
-        glGenVertexArrays(1, &mut vertex_array_id);
-        if vertex_array_id == 0 { panic!("Invalid vertex array!"); }
-        glBindVertexArray(vertex_array_id);
-        check_error().context("Cannot bind buffer")?;
-
-        // vertex location
-        glEnableVertexAttribArray(vertex_loc as u32);
-        glVertexAttribPointer(vertex_loc as u32, 3, GL_FLOAT, GL_FALSE as u8, 20, null());
-        check_error().context("Cannot set vertex attrib pointer")?;
-
-        // texture attribute
-        glEnableVertexAttribArray(texcoord_loc as u32);
-        check_error().context("Cannot enable vertex attrib array")?;
-        let num = 12;
-        glVertexAttribPointer(texcoord_loc as u32, 2, GL_FLOAT, GL_FALSE as u8, 20, num as *const c_void);
-        check_error().context("Cannot set texture attrib pointer")?;
-
-        // Build the framebuffer.
-        let mut framebuffer: GLuint = 0;
-        info!("Generate framebuffer...");
-        glGenFramebuffers(1, &mut framebuffer);
-        info!("Bind framebuffer...");
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-        info!("Applying texture as framebuffer...");
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, yout, 0);
-        info!("Checking framebuffer status...");
-        let status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-        if status != GL_FRAMEBUFFER_COMPLETE {
-            error!("Error checking framebuffer: {}", status);
-            return Err(anyhow!("Unable to set framebuffer"));
-        }
-
-        // clear
-        glViewport(0, 0, width, height);
-        glClearColor(0.0, 0.0, 0.0, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
-        check_error().context("Cannot clear!")?;
-
-        // draw
-        glUseProgram(program_id);
-        glPixelStorei(GL_PACK_ALIGNMENT, 1);
-        glValidateProgram(program_id);
-        let mut res: GLint = 0;
-        glGetProgramiv(program_id, GL_VALIDATE_STATUS, &mut res as *mut GLint);
-        if res == 0 { panic!("Bad program") }
-        check_error().expect("Invalid program!");
-
-        glDrawArrays(GL_TRIANGLES, 0, (vertex_buffer_data.len() / 3) as i32);
-        check_error().context("Cannot draw!")?;
-        glFinish();
-        check_error().context("Cannot finish!")?;
-
-        let mut pixels = vec![0u8; (width * height) as usize * 4];
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        check_error().context("Cannot set pixel store mode!")?;
-        glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.as_mut_ptr() as *mut c_void);
-        check_error().context("Cannot get pixels!")?;
-
-        // Save
-        let path = format!("{}/pic{}.raw", path, idx);
-        info!("Writing file {}...", path);
-        let mut file = File::create(path)?;
-        file.write_all(&pixels[..])?;
+        let mut data_buffer: GLuint = 0;
+        let data: Vec<u32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        glGenBuffers(1, &mut data_buffer);
+        glBindBuffer(GL_SHADER_STORAGE_BUFFER, data_buffer);
+        glBufferData(GL_SHADER_STORAGE_BUFFER, size_of::<u32>() as i64 * 10, data.as_ptr() as *const c_void, GL_STREAM_COPY);
+        info!("it worked!");
     }
-
     Ok(())
 }
-
-pub unsafe fn make_shader(vertex_source: &str, fragment_source: &str) -> Result<u32, Error> {
-    let mut program_ok: GLint = 0;
-    let vertex_shader = make_shader_object(GL_VERTEX_SHADER, vertex_source).context("Cannot compile vertex shader")?;
-    let fragment_shader = make_shader_object(GL_FRAGMENT_SHADER, fragment_source).context("Cannot compile fragment shader")?;
-    if vertex_shader == 0 || fragment_shader == 0 {
-        return Err(anyhow!("Failed to make shader!"));
-    }
-    let program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-
-    glLinkProgram(program);
-    glGetProgramiv(program, GL_LINK_STATUS, &mut program_ok);
-    if program_ok == 0 {
-        glDeleteShader(vertex_shader);
-        glDeleteShader(fragment_shader);
-        glDeleteProgram(program);
-        return Err(anyhow!("Failed to link shader program"));
-    }
-    return Ok(program);
-}
-
-pub unsafe fn make_shader_object(shader_type: GLenum, source: &str) -> Result<GLuint, Error> {
-    let string_ptr = source.as_ptr() as *const GLchar;
-    let mut shader: GLuint = 0;
-    let mut shader_ok: GLint = 0;
-    shader = glCreateShader(shader_type);
-    glShaderSource(shader, 1, &string_ptr as *const *const GLchar, null());
-    glCompileShader(shader);
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &mut shader_ok);
-    if shader_ok == 0 {
-        glDeleteShader(shader);
-        check_error().context("Failed to compile")?;
-        return Err(anyhow!("Failed to compile!"));
-    }
-    return Ok(shader);
-}
-
-pub unsafe fn check_error() -> Result<(), anyhow::Error> {
-    let err: GLenum = glGetError();
-    match err {
-        0 => return Ok(()),
-        GL_INVALID_ENUM => Err(anyhow!("GL error: Invalid enum")),
-        GL_INVALID_VALUE => Err(anyhow!("GL error: Invalid value")),
-        GL_INVALID_OPERATION => Err(anyhow!("GL error: Invalid operation")),
-        GL_OUT_OF_MEMORY => Err(anyhow!("GL error: Out of memory")),
-        GL_INVALID_FRAMEBUFFER_OPERATION => Err(anyhow!("GL error: invalid frame buffer")),
-        _ => Err(anyhow!("GL error: Unknown {:#x}", err))
-    }
-}
-
